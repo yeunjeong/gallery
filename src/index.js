b18e892203cfcc5cfa0e8571985ed15c59740b2d
yeunjeong <araruu2536@gmail.com>
2023-08-29 15:30:15 +0900

    scroll to top

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import ScrollToTop from "./components/ScrollToTop";
 import "./Fonts.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>
