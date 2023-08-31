a18b1b3a83b154781e3c2dc6da4d51c9be0f0d12
yeunjeong <araruu2536@gmail.com>
2023-08-31 21:30:55 +0900

    activity

---
+++ src/components/WorkCBox.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./WorkCBox.module.css";
+
+function WorkCBox({ id, title, image, showDetail }) {
+  return (
+    <div
+      className={`${styles.projectBox} ${
+        id % 2 === 0 ? styles.marginStyle : ""
+      }`}
+    >
+      <div className={styles.titleText}>{title}</div>
+      <div className={styles.imageWrapper} onClick={showDetail}>
+        <img src={image} alt="project" className={styles.imageStyle}></img>
+      </div>
+      <div className={styles.textBox}>
+        <div>#GOOD_THING #GOOD_THING</div>
+        <div>#GOOD_THING #GOOD_THING</div>
+        <div>#GOOD_THING #GOOD_THING</div>
+      </div>
+    </div>
+  );
+}
+
+WorkCBox.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  showDetail: PropTypes.func.isRequired,
+};
+
+export default WorkCBox;
--- src/routes/WorkC.js
+++ src/routes/WorkC.js
@@ -1,10 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./WorkC.module.css";
+import ProjBox from "../components/WorkCBox";
+import Footer from "../components/Footer";
 
 function WorkC() {
+  const data = [
+    {
+      img: require("../assets/images/works/works_a/project_a.png"),
+      title: "ACTIVITY A",
+    },
+    {
+      img: require("../assets/images/works/works_c/activity_b.png"),
+      title: "ACTIVITY B",
+    },
+    {
+      img: require("../assets/images/works/works_c/activity_c.png"),
+      title: "ACTIVITY C",
+    },
+    {
+      img: require("../assets/images/works/works_c/activity_d.png"),
+      title: "ACTIVITY D",
+    },
+    {
+      img: require("../assets/images/works/works_c/activity_e.png"),
+      title: "ACTIVITY E",
+    },
+    {
+      img: require("../assets/images/works/works_c/activity_f.png"),
+      title: "ACTIVITY F",
+    },
+    {
+      img: require("../assets/images/works/works_c/activity_g.png"),
+      title: "ACTIVITY G",
+    },
+    {
+      img: require("../assets/images/works/works_c/activity_h.png"),
+      title: "ACTIVITY H",
+    },
+    {
+      img: require("../assets/images/works/works_c/activity_i.png"),
+      title: "ACTIVITY I",
+    },
+    {
+      img: require("../assets/images/works/works_c/activity_j.png"),
+      title: "ACTIVITY J",
+    },
+  ];
+
+  const [isDetailOpen, setIsDetailOpen] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const showDetail = (id) => {
+    setCurrentImageIndex(id);
+    setIsDetailOpen(true);
+
+    // Scroll to the top of the page
+    window.scrollTo(0, 0);
+  };
+
+  const hideDetail = () => {
+    setIsDetailOpen(false);
+  };
+
   return (
     <div>
-      <div className={styles.header}>WorkC</div>
+      {isDetailOpen ? (
+        <div className={styles.detailBox} onClick={hideDetail}></div>
+      ) : (
+        <div className={styles.contentBox}>
+          <div style={{ marginLeft: "5vh" }}>ACTIVITY</div>
+          <div className={styles.projectBox}>
+            {[0, 2, 4, 6, 8].map((index) => (
+              <div
+                style={{ display: "flex", marginBottom: "35px" }}
+                key={index}
+              >
+                <ProjBox
+                  id={index}
+                  title={data[index].title}
+                  image={data[index].img}
+                  showDetail={showDetail}
+                />
+                <ProjBox
+                  id={index + 1}
+                  title={data[index + 1].title}
+                  image={data[index + 1].img}
+                  showDetail={showDetail}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      <Footer />
     </div>
   );
 }
